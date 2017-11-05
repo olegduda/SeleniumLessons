a9ef2e4f0a458ef4d7bc84f3d621c5146a466514
oleg.duda <om8om@mail.ru>
2017-11-05 20:57:01 +0300

    with POM

--- google_search/google_js_qa.py
+++ google_search/google_js_qa.py
@@ -3,61 +3,30 @@
 # google search
 
 import unittest
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+
+from .google_pages import *
 
 class testGoogleSearch(unittest.TestCase):
 
-    PAGE_LOAD = "http://google.com/"
-
-    def wait_for_page_to_load(self, timeout=10000):
-        w = WebDriverWait(self.driver, timeout / 100.0)
-        w.until(lambda driver: driver.execute_script("return document.readyState") == "complete")
-
     def setUp(self):
         self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(18)
-        self.driver.get(self.PAGE_LOAD)
+        self.home_page = HomePage(self.driver)
 
     def tearDown(self):
         self.driver.quit()
 
     def test_search(self):
-        # Width of the search field
-        input_width = self.driver.execute_script("return document.getElementById('lst-ib').offsetWidth;")
+        self.home_page.open()
+        self.home_page.width_input_field()
+        self.home_page.is_scloll_page()
+        self.home_page.input_text()
+        self.home_page.off_focus()
+        self.home_page.click_page()
 
-        # Is a scrolling page
-        is_scroll_page = self.driver.execute_script("return document.body.clientHeight-document.documentElement.clientHeight;")
-
-        # Enter the query
-        input_str_search = self.driver.execute_script("document.getElementById('lst-ib').value='Selenium';")
-
-        # Take off focus
-        input_str_search = self.driver.execute_script("document.getElementById('lst-ib').blur;")
-
-        # Perform a search
-        input_str_search = self.driver.execute_script("document.getElementById('_fZl').click();")
-
-        # Verifying page loading
-        WebDriverWait(self.driver, 8).until(lambda driver: self.driver.execute_script("return document.getElementById('navcnt');"))
-
-        is_scroll_page = self.driver.execute_script("return document.body.clientHeight-document.documentElement.clientHeight;")
-
-
+        assert (self.home_page.verify_load_page()), "Page not load"
+        assert (self.home_page.is_scloll_page()), "Page not scroll"
 
 if __name__ == "__main__":
     unittest.main()
-
-'''
-function get_scroll(a) {
-   var d = document,
-        b = d.body,
-        e = d.documentElement,
-        c = "client" + a;
-        a = "scroll" + a;
-   return /CSS/.test(d.compatMode)? (e[c]< e[a]) : (b[c]< b[a])
-};
-get_scroll('Height')
-'''
 
 
--- google_search/google_pages.py
+++ google_search/google_pages.py
@@ -2,3 +2,49 @@
 
 # google pages
 
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+PAGE_LOAD = "http://google.com/"
+TEXT_SEARCH = "Selenium and Python"
+
+class HomePage():
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.driver.implicitly_wait(16)
+
+    def open(self, page_load=PAGE_LOAD):
+        self.driver.get(page_load)
+        return self
+
+    def width_input_field(self):
+        input_width = self.driver.execute_script("return document.getElementById('lst-ib').offsetWidth;")
+        return input_width
+
+    def is_scloll_page(self):
+        is_scroll = self.driver.execute_script("return document.body.clientHeight-document.documentElement.clientHeight;")
+        if is_scroll == 0:
+            is_scroll = False
+        else:
+            is_scroll = True
+        return is_scroll
+
+    def input_text(self,text_search=TEXT_SEARCH):
+        self.driver.execute_script("document.getElementById('lst-ib').value='" + text_search +"';")
+
+    def off_focus(self):
+        self.driver.execute_script("document.getElementById('lst-ib').blur;")
+
+    def verify_load_page(self):
+        try:
+            WebDriverWait(self.driver, 8).until(
+                lambda driver: self.driver.execute_script("return document.getElementById('navcnt');"))
+            is_load = True
+        except:
+            is_load = False
+        return is_load
+
+    def click_page(self):
+        self.driver.execute_script("document.getElementById('_fZl').click();")
+
